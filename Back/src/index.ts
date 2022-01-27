ca3c15fb69339142979811f437e2fb535fc17ec2
Rom <romkadria@gmail.com>
2022-01-27 22:29:24 +0200

    mongo connection + get all hobbies

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -1,7 +1,24 @@
 import express from "express";
+import { getAllHobbies } from "./MongoDb/Hobbies/Actions";
 
 const app = express();
 const port = 3000;
+
+app.get("/getAllHobbies", (req, res) => {
+  res.send(getAllHobbies());
+  res.json("ok");
+})
+// async function mainDb() {
+
+//   const hobbies = await getAllHobbies();
+//   console.log(hobbies);
+
+//   if(!hobbies) {
+//     console.log("no hobbies have been returned");
+//   }
+// }
+
+// mainDb();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
