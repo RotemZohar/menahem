ae12e4103be36c30308e4340dfd9a2c95f840b9d
rotem <rzrotem@gmail.com>
2022-01-27 22:22:38 +0200

    Add cors

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -1,10 +1,17 @@
+import cors from "cors";
 import express from "express";
 
 const app = express();
 const port = 4000;
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/add-user", (req, res) => {
+  console.log(req.body);
+  res.json("OK");
 });
 
 app.listen(port, () => {
