c6f544fb4ac4f0d5385622eb8888f70ad2b94ce3
rotem <rzrotem@gmail.com>
2022-01-28 14:38:04 +0200

    Add user slice

--- Front/src/redux/store.ts
+++ Front/src/redux/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./slices/userSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { userReducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
