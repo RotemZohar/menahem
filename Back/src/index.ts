04e98dec9fd7e088f0567479600cf8d412f03a86
Eran <eran@nizry.com>
2022-02-04 16:32:19 +0200

    Added post actions

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -1,7 +1,13 @@
 import cors from "cors";
 import express from "express";
+import { ObjectId } from "mongodb";
 import { getAllHobbies, getHobbie } from "./MongoDb/Hobbies/Actions";
-import { getPostByTag } from "./MongoDb/Posts/Actions";
+import {
+    getPostByTag,
+    addPost,
+    editPost,
+    deletePost,
+} from "./MongoDb/Posts/Actions";
 import { addUser, getUser } from "./MongoDb/Users/Actions";
 
 const app = express();
@@ -13,41 +19,70 @@ app.use(express.json());
 
 // Hobbies
 app.get("/hobbies/getAll", async (req, res) => {
-  const hobbies = await getAllHobbies();
-  res.send(hobbies);
+    const hobbies = await getAllHobbies();
+    res.send(hobbies);
 });
 
 app.get("/hobbies/:id", async (req, res) => {
-  const hobbie = await getHobbie(req.params.id);
-  res.send(hobbie);
+    const hobbie = await getHobbie(req.params.id);
+    res.send(hobbie);
 });
 
 // Posts
 app.get("/posts/byTag/:tag", async (req, res) => {
-  const posts = await getPostByTag(req.params.tag);
-  if (posts) {
-    res.send(posts);
-  } else {
-    res.json("no values for this tag");
-  }
+    const posts = await getPostByTag(req.params.tag);
+    if (posts) {
+        res.send(posts);
+    } else {
+        res.json("no values for this tag");
+    }
+});
+
+app.post("/posts/add", async (req, res) => {
+    const post = req.body;
+    const result = await addPost(post);
+    if (result) {
+        res.send(result);
+    } else {
+        res.send("Addition failed");
+    }
+});
+
+app.put("/posts/:id", async (req, res) => {
+    const post = req.body;
+    const result = await editPost(new ObjectId(req.params.id), post);
+    if (result) {
+        res.send(result);
+    } else {
+        res.send("Editing failed");
+    }
+});
+
+app.delete("/posts/:id", async (req, res) => {
+    const result = await deletePost(new ObjectId(req.params.id));
+    if (result) {
+        res.send("Deletion succeeded");
+    } else {
+        res.send("Deletion failed");
+    }
 });
 
 // Users
 app.post("/users/add", async (req, res) => {
-  const user = req.body;
-  const result = await addUser(user);
-  if (result) {
-    res.send(user.email);
-  } else {
-    res.send("adding failed");
-  }
+    const user = req.body;
+    const result = await addUser(user);
+    if (result) {
+        res.send(user.email);
+    } else {
+        res.send("adding failed");
+    }
 });
 
 app.get("/users/get:email", async (req, res) => {
-  const user = await getUser(req.params.email);
-  res.send(user);
+    const user = await getUser(req.params.email);
+    res.send(user);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
