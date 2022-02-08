63a376c70d0a776976838e499b81e6a8391a89b6
michal-zv <michal-zv>
2022-02-09 00:22:04 +0200

    added users table

--- Front/src/components/admin-page/AdminPage.tsx
+++ Front/src/components/admin-page/AdminPage.tsx
@@ -1,47 +1,48 @@
 import { Box, Tab, Tabs, Typography } from "@mui/material";
 import React from "react";
+import UsersTable from "./UsersTable";
 
-function TabPanel(props: { index: number, value: number, children: any }) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
-  export default function AdminPage() {
-    const [value, setValue] = React.useState(0);
-  
-    const handleChange = (event: any, newValue: any) => {
-      setValue(newValue);
-    };
-  
-    return (
-      <Box sx={{ width: '100%' }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange}>
-            <Tab label="Posts" />
-            <Tab label="Users" />
-          </Tabs>
+function TabPanel(props: { index: number; value: number; children: any }) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
         </Box>
-        <TabPanel value={value} index={0}>
-          Item One
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          Item Two
-        </TabPanel>
+      )}
+    </div>
+  );
+}
+
+export default function AdminPage() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: any, newValue: any) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Posts" />
+          <Tab label="Users" />
+        </Tabs>
       </Box>
-    );
-  }
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <UsersTable />
+      </TabPanel>
+    </Box>
+  );
+}
